05f8d516ab77808136d683eb2ae9e033f8e9104e
Cj <cj@Cjs-MacBook-Pro.local>
2017-12-11 17:29:55 -0800

    restaurant form

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import UserForm, RestaurantForm
 # Create your views here.
 def home(request):
     return redirect(restaurant_home)
@@ -9,4 +10,10 @@ def restaurant_home(request):
     return render(request, 'restaurant/home.html',{})
 
 def restaurant_sign_up(request):
-    return render(request, 'restaurant/sign_up.html',{})
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+    context = {
+        'user_form':user_form,
+        'restaurant_form':RestaurantForm,
+    }
+    return render(request, 'restaurant/sign_up.html',context)
