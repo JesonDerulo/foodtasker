ebcc066f5866eb9cae97950597a15223cc34f6a9
Cj <cj@Cjs-MacBook-Pro.local>
2017-12-11 17:18:44 -0800

    add resturant sign-up

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+class Restaurants(models.Model):
+    user        = models.OneToOneField(User, on_delete=models.CASCADE, related_name='restaurant')
+    name        = models.CharField(max_length=500)
+    phone       = models.CharField(max_length=500)
+    address     = models.CharField(max_length=500)
+    logo        = models.ImageField(upload_to='restaurant_logo/', blank=True)
 
+    def __str__(self):
+        return self.name
 
 
